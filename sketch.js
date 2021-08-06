df97a8cfab98ba5c76bae23cdaee2f91f466e950
Adrika <adrika18omh0010@dsreduin.onmicrosoft.com>
2021-08-06 23:22:18 +0530

    Nice

--- sketch.js
+++ sketch.js
@@ -1,57 +1,121 @@
 var bg, bgImg;
+var happy, hImg, move, m, y, thIm, restart, reImg;
+var gameState = 1;
+var cool, pol;
 
 function preload() {
+  reImg = loadImage("reImg.jpg")
+  thIm = loadImage("th.jpg");
+  hImg = loadImage("Happy.jpg");
+  m = loadImage("MOVE.jpg");
   bg  = loadImage("459.jpg");
-  thiefImg = loadImage("theif.webp")
-  copImg = loadImage("cop2.jpg")
+  thiefImg = loadImage("theif.webp");
+  copImg = loadImage("cop2.webp");
+  bg2 = loadImage("bg2.jpg");
+
+  soundFormats('mp3', 'ogg');
+  cool = loadSound("o.mp3");
+  pol = loadSound("p.mp3");
+
 }
 
 function setup() {
-  createCanvas(500, 500);
- bgImg = createSprite(0, 0, 500, 500);
+  createCanvas(700, 500);
+ bgImg = createSprite(200, 200, 500, 500);
  bgImg.addImage(bg);
 
- cop = createSprite(100, 180, 20, 20);
+ cop = createSprite(100, 400, 20, 20);
  cop.addImage(copImg);
- cop.scale = 0.3
+ cop.scale = 0.11;
 
- theif = createSprite(400, 200, 20, 20);
+ restart = createSprite(650, 50, 20, 20);
+ restart.addImage(reImg);
+ restart.scale = 0.15;
+ restart.visible = false;
+
+
+ theif = createSprite(400, 430, 20, 20);
  theif.addImage(thiefImg);
  theif.scale = 0.15;
  theif.velocityX = 2.5 ;
+
+ happy = createSprite(250, 270, 50, 50);
+ happy.addImage(hImg);
+ happy.scale = 0.5;
+ happy.visible = false;
+
+ move = createSprite(150, 25, 50, 50);
+ move.addImage(m);
+ move.scale = 0.5;
+
+ y = createSprite(250, 150, 50, 50);
+ y.addImage(thIm);
+ y.scale = 0.5;
+ y.visible = false;
 }
 
-function draw() {
-
-//  fill("Black")
-//  text("Use Arrow Keys to MOVE", 50, 50)
-
-  bgImg.velocityX = -2;
+function draw(){
 
   if (bgImg.x < 0) {
     bgImg.x = bgImg.width/2;
   }
 
- if(keyDown(RIGHT_ARROW)){
-   cop.x = cop.x + 3;
- }
+  if (gameState = 1) {
+  
+   if(keyDown(RIGHT_ARROW)){
+     cop.x = cop.x + 3;
+   }
+  
+   if(keyDown(LEFT_ARROW)){
+    cop.x = cop.x - 3;
+  }
+   
+    if (theif.x > 700) {
+      theif.x = 800;
+    }
 
- if(keyDown(UP_ARROW)){
-  cop.x = cop.x - 3;
-}
- 
-  if (theif.x > 500) {
-    theif.x = 550;
+    if(theif.x < 0){
+      theif.x = 600
+      theif.velocityX = 2;
+    }
+  
+    if (cop.x === 400) {
+        theif.x = 100;
+        theif.velocityX = -2;
+    }
+
+    if (cop.isTouching(theif)){
+      gameState = 0; 
+    } 
+
+    pol.play();
+  } 
+  if(gameState === 0){
+    theif.velocityX = 0;
+    theif.visible = false;
+    cop.x = 250;
+    restart.visible = true;
+    happy.visible = true;
+    y.visible = true;
+
+    cool.play();
+ //   pol.pause();
   }
 
-  if (theif.x < -5) {
-    theif.x = 100;
-  }
-
-  if (cop.x === 400) {
-      theif.x = 200;
-      theif.velocityX = -2;
+  if(mousePressedOver(restart)){
+    reset();
   }
 
   drawSprites();
+}
+
+function reset(){
+  gameState = 1;
+  happy.visible = false;
+  theif.visible = true;
+  theif.velocityX = 2;
+  y.visible = false;
+  restart.visible = false;
+  cop.x = 100;
+  pol.play();
 }
